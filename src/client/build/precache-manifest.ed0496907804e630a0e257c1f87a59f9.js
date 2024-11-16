b44cbf8b8b563c3aea5d16ccd24e6f088bf83ee3
Guy Manor <guymanor5@gmail.com>
2024-11-16 13:28:34 +0200

    big bang

---
+++ src/client/build/precache-manifest.ed0496907804e630a0e257c1f87a59f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eded8761d6f39e0f3931363ac65e3b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f12721fafd7d4c70e5d",
+    "url": "/static/css/main.c3618a84.chunk.css"
+  },
+  {
+    "revision": "50acfa049c4728d68e95",
+    "url": "/static/js/2.e1c12a49.chunk.js"
+  },
+  {
+    "revision": "2f12721fafd7d4c70e5d",
+    "url": "/static/js/main.44c23520.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
