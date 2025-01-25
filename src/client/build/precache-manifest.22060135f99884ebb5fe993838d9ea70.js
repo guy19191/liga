a14a8b363f6ae6b06492189986496d621342bdd7
Guy Manor <guymanor5@gmail.com>
2025-01-25 23:09:12 +0200

    fix profie

---
+++ src/client/build/precache-manifest.22060135f99884ebb5fe993838d9ea70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae7d47be75993788f6fc31b1524d46ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9537e1408bde28c1a473",
+    "url": "/static/css/main.cd04e6db.chunk.css"
+  },
+  {
+    "revision": "a2e0298ebb12500bb0f1",
+    "url": "/static/js/2.b7771d8b.chunk.js"
+  },
+  {
+    "revision": "9537e1408bde28c1a473",
+    "url": "/static/js/main.13a301d9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
